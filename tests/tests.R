5d2b744225e4e5276b1045c2dbc3142d53662b63
Ajeya Kempegowda <ajeyakempegowda@Ajeyas-MacBook-Pro.local>
2020-11-04 17:39:53 -0500

    fixed paws bug

--- tests/tests.R
+++ tests/tests.R
@@ -8,7 +8,7 @@ if (!requireNamespace("devtools", quietly = TRUE))
     install.packages("devtools")
 
 if (!requireNamespace("paws", quietly = TRUE))
-    install.packages("aws.s3")
+    install.packages("paws")
 
 BiocManager::install("MSstats")
 devtools::install_github("Vitek-Lab/MSstats-dev", ref = "refactoring")
