79866964a1b4e1cab4792cd4728e3f02be05004a
Ajeya Kempegowda <ajeyakempegowda@Ajeyas-MacBook-Pro.local>
2020-11-04 16:29:17 -0500

    Removed aws.s3 and incorporated paws package

--- tests/tests.R
+++ tests/tests.R
@@ -6,29 +6,31 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 if (!requireNamespace("devtools", quietly = TRUE))
     install.packages("devtools")
 
-if (!requireNamespace("aws.s3", quietly = TRUE))
-    install.packages("aws.s3", repos = "https://cloud.R-project.org")
+if (!requireNamespace("paws", quietly = TRUE))
+    install.packages("aws.s3")
 
 BiocManager::install("MSstats")
 devtools::install_github("Vitek-Lab/MSstats-dev", ref = "refactoring")
 
-library(aws.s3)
 source("/home/rstudio/code/config.R")
 
-bucket <- aws.s3::get_bucket(
+## store the aws credentials as environment variables
+s3_bucket <- paws::get_bucket(
     bucket = aws_bucket_name,
     key = aws_key,
     secret = aws_secret
 )
 
-# `load()` R objects from the file
-frags = aws.s3::s3readRDS(object = "Navarro2016_DIA_DIAumpire_input_FragSummary.RDS", bucket = "nu-ov-lab-datasets")
-pepts = aws.s3::s3readRDS(object = "Navarro2016_DIA_DIAumpire_input_PeptideSummary.RDS", bucket = "nu-ov-lab-datasets")
-prots = aws.s3::s3readRDS(object = "Navarro2016_DIA_DIAumpire_input_ProtSummary.RDS", bucket = "nu-ov-lab-datasets")
-dia_annot = aws.s3::s3readRDS(object = "Navarro2016_DIA_DIAumpire_input_annotation.RDS", bucket = "nu-ov-lab-datasets")
-diau = MSstatsdev::DIAUmpiretoMSstatsFormat(frags, pepts, prots, dia_annot)
+# `load()` R objects from S3
+frags = s3_bucket$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_FragSummary.RDS")
+pepts = s3_bucket$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_PeptideSummary.RDS")
+prots = s3_bucket$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_ProtSummary.RDS")
+dia_annot = s3_bucket$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_annotation.RDS")
 
+diau = MSstatsdev::DIAUmpiretoMSstatsFormat(frags, pepts, prots, dia_annot)
 diau_v3 = MSstats::dataProcess(as.data.frame(unclass(diau)), MBimpute = TRUE)
 diau_v4 = MSstatsdev::dataProcess(diau, MBimpute = TRUE)
 
-aws.s3::s3saveRDS(x = diau_v4, bucket = aws_bucket_name, object = "from-ec2.rds")
+# to check if we can store the rds object to S3
+s3$put_object(Body = diau_v4, Bucket = aws_bucket_name, Key = "from-ec2.rds")
+# aws.s3::s3saveRDS(x = diau_v4, bucket = aws_bucket_name, object = "from-ec2.rds")
