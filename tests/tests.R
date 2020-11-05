5b9c72287afdcd07afa67459afd8325df2505270
Ajeya Kempegowda <ajeyakempegowda@Ajeyas-MacBook-Pro.local>
2020-11-04 19:53:02 -0500

    updated paws logic

--- tests/tests.R
+++ tests/tests.R
@@ -13,20 +13,25 @@ if (!requireNamespace("paws", quietly = TRUE))
 BiocManager::install("MSstats")
 devtools::install_github("Vitek-Lab/MSstats-dev", ref = "refactoring")
 
+#load all the config settings/credentials
 source("/home/rstudio/code/config.R")
 
-## store the aws credentials as environment variables
-s3_bucket <- paws::get_bucket(
-    bucket = aws_bucket_name,
-    key = aws_key,
-    secret = aws_secret
+#set aws session variables
+Sys.setenv(
+    AWS_ACCESS_KEY_ID = aws_key,
+    AWS_SECRET_ACCESS_KEY = aws_secret,
+    AWS_REGION = aws_region
 )
+#initialise s3 object
+s3 <- paws::s3()
+# quick smoke test to check bucket contents
+s3$list_buckets()
 
 # `load()` R objects from S3
-frags = s3_bucket$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_FragSummary.RDS")
-pepts = s3_bucket$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_PeptideSummary.RDS")
-prots = s3_bucket$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_ProtSummary.RDS")
-dia_annot = s3_bucket$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_annotation.RDS")
+frags = s3$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_FragSummary.RDS")
+pepts = s3$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_PeptideSummary.RDS")
+prots = s3$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_ProtSummary.RDS")
+dia_annot = s3$get_object(Bucket = aws_bucket_name,Key = "Navarro2016_DIA_DIAumpire_input_annotation.RDS")
 
 diau = MSstatsdev::DIAUmpiretoMSstatsFormat(frags, pepts, prots, dia_annot)
 diau_v3 = MSstats::dataProcess(as.data.frame(unclass(diau)), MBimpute = TRUE)
